d4e650c0231a012c43632d149210ca52273b0fb6
kevin <acosta.kevin@correounivalle.edu.co>
2023-06-28 17:04:57 -0500

    implementada pilaConColas

---
+++ punto3/pilaconcola.py
@@ -0,0 +1,104 @@
+"""
+Autores: Kevin Acosta
+         Ronan Moreno
+         Rodolfo Bolaños
+fecha: 28/06/2023
+Descripción: Clase que representa una cola
+"""
+import numpy as np
+
+
+class PilaConCola:
+
+    def __init__(self, n):
+        self.size = 0
+        self.queue = np.zeros(n)
+        self.head = 1
+        self.tail = 1
+        self.n = n
+
+    def enqueue(self, value):
+        if self.tail == self.head and self.size > 0:
+            raise NotImplementedError("La cola esta llena")
+        else:
+            self.size += 1
+            self.queue[self.tail - 1] = value
+            if self.tail == self.n:
+                self.tail = 1
+            else:
+                self.tail += 1
+
+    def dequeue(self):
+        if self.tail == self.head and self.size == 0:
+            raise NotImplementedError("La cola esta vacia")
+        else:
+            self.size -= 1
+            val = self.queue[self.head - 1]
+            if self.head == self.n:
+                self.head = 1
+            else:
+                self.head += 1
+
+        return val
+
+    """"
+    las lienas siguientes se implementan funciones que individualmente funcionan 
+    como pila en esta cola osea si encolamos y hacemos pop el ultimo que entre es 
+    el primero que sale
+    
+    si hacemos push y desencolamos el primero que entre es el ultimo que sale
+    """
+
+    def estaVaciaPilaConColas(self):
+        if self.size == 0:
+            return True
+        else:
+            return False
+
+    def pushPilaConColas(self, value):
+        if self.tail == self.head and self.size > 0:
+            raise NotImplementedError("La cola esta llena")
+        else:
+            self.size += 1
+            # por la estructura de los array de numpy puedo poner
+            # el valor en la pos -1 que sería igual
+            # a la ultima posicion cuando la cabeza esté al principio
+            self.queue[self.head - 2] = value  # agrega el valor antes de la cabeza
+            if self.head == 1:   # con este condicional pongo al head en la posicion del valor
+                self.head = self.n
+            else:
+                self.head -= 1
+
+    def popPilaConColas(self):
+        # elimino el ultimo que ingresó
+        if self.tail == self.head and self.size == 0:
+            raise NotImplementedError("La cola esta vacia")
+        else:
+            self.size -= 1
+            val = self.queue[self.tail - 2]
+            if self.tail == 1:
+                self.tail = self.n
+            else:
+                self.tail -= 1
+
+        return val
+    """
+    mostraremos la pila con colas como se mostraría una lista
+    """
+    def mostrarPilaConColas(self):
+        if self.tail == self.head and self.size == 0:
+            print("[]")
+        else:
+            if self.tail > self.head:
+                print("[")
+                for i in range(self.head - 1, self.tail - 1):
+                    print(self.queue[i], end=" ")
+                print("]")
+            else:
+                print("[")
+                for i in range(self.head - 1, self.n):
+                    print(self.queue[i], end=" ")
+                for i in range(0, self.tail - 1):
+                    print(self.queue[i], end=" ")
+                print("]")
+            print()
--- punto3/test_cola.py
+++ punto3/test_cola.py
@@ -3,13 +3,14 @@ Autores: Kevin Acosta
          Ronan Moreno
          Rodolfo Bolaños
 fecha: 28/06/2023
-Descripción: archivo de tests para la clase Cola
+Descripción: archivo de tests para la clase PilaConCola
+en esta implementaremos funciones inddividuales que se comportan
 """
-from cola import Cola
+from pilaconcola import PilaConCola
 
 
 def test_enqueue_and_dequeue():
-    cola = Cola(5)
+    cola = PilaConCola(5)
     cola.enqueue(1)
     cola.enqueue(2)
     cola.enqueue(3)
@@ -32,3 +33,72 @@ def test_enqueue_and_dequeue():
         assert True
     else:
         assert False
+
+
+def test_estaVaciaPilaConColas():
+    cola = PilaConCola(5)
+    assert cola.estaVaciaPilaConColas() is True
+    cola.enqueue(1)
+    assert cola.estaVaciaPilaConColas() is False
+    cola.dequeue()
+    assert cola.estaVaciaPilaConColas() is True
+
+
+def test_pushPilaConColas():
+    # con push se ponen los valores atrás de la cabeza
+    # y luego se actualiza la cabeza así cuando los saque
+    # sacaré el último valor que entró con dequeue
+    cola = PilaConCola(5)
+    cola.enqueue(1)
+    cola.pushPilaConColas(3)
+    cola.pushPilaConColas(3)
+    cola.pushPilaConColas(6)    # ultimo en entrar
+
+    assert cola.dequeue() == 6  # primero en salir
+    assert cola.dequeue() == 3
+    assert cola.dequeue() == 3
+    assert cola.dequeue() == 1
+
+
+def test_popPilaConColas():
+    cola = PilaConCola(5)
+    cola.enqueue(1)
+    cola.enqueue(2)
+    cola.enqueue(3)
+    cola.enqueue(4)
+    cola.enqueue(5)  # ultimo en entrar
+
+    assert cola.popPilaConColas() == 5  # primero en salir
+    assert cola.popPilaConColas() == 4
+    assert cola.popPilaConColas() == 3
+    assert cola.popPilaConColas() == 2
+    assert cola.popPilaConColas() == 1
+    try:
+        cola.popPilaConColas()
+    except NotImplementedError:  # La cola esta vacia
+        assert True
+    else:
+        assert False
+
+
+def test_mostrarPilaConColas():
+    cola = PilaConCola(5)
+    cola.enqueue(1)
+    cola.enqueue(2)
+    cola.enqueue(3)
+    cola.enqueue(4)
+    cola.enqueue(5)  # ultimo en entrar
+    cola.mostrarPilaConColas()
+
+    cola1 = PilaConCola(5)
+    cola1.pushPilaConColas(1)
+    cola1.pushPilaConColas(2)
+    cola1.pushPilaConColas(3)
+    cola1.pushPilaConColas(4)
+    cola1.pushPilaConColas(5)
+    cola1.mostrarPilaConColas()
+
+
+
+
+
